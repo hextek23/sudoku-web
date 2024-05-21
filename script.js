b298f10cd3706a9c4c4899645c5ea256293ddc0f
hextek23 <s153314@ap.be>
2024-05-21 23:23:51 +0200

    js code to generate random 9x9 matrix and html value insert

--- script.js
+++ script.js
@@ -3,4 +3,28 @@ function isNumberKey(evt) {
     if (charCode > 31 && (charCode < 48 || charCode > 57))
       return false;
     return true;
+}
+
+function generateSudokuGrid() {
+  const grid = [];
+  for (let row = 0; row < 9; row++) {
+      const currentRow = [];
+      for (let col = 0; col < 9; col++) {
+          const randomNum = Math.floor(Math.random() * 9) + 1;
+          currentRow.push(randomNum);
+      }
+      grid.push(currentRow);
   }
+  return grid;
+}
+
+function displayGridOnSite(){
+  const sudokuGrid = generateSudokuGrid();
+  const cell1 = document.querySelector(".s1 .g1");
+  cell1.value = sudokuGrid[0][0];
+  console.log(cell1.value);
+  return sudokuGrid;
+}
+
+const sudokuGrid = displayGridOnSite();
+console.log(sudokuGrid);
