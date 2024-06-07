0da4aad7b7900372ab1a4d4bcb46c7f993ad341b
hextek23 <s153314@ap.be>
2024-06-07 14:26:28 +0200

    diagonal generation and full grid display

--- script.js
+++ script.js
@@ -29,92 +29,126 @@ function generateRandom9(){
 function generateSudokuGrid() {//generates a full sudoku grid 9x9
   
   var grid = []; //9x9 the box(3x3) are the rows of this grid
-  
-  // for (let i = 0; i < 9; i++){
-  //   if(i == 0 || i == 4 || i == 8){// this generates the diagonals of the sudoku grid these are independant of each other wich means that you can generate these randomly
-  //     var arr = generateRandom9();
-  //     grid.push(arr);
-  //   }
-  //   else{
-  //     grid.push([0,0,0,0,0,0,0,0,0])
-  //   }
-  // }
+
+  for (let i = 0; i < 9; i++){
+    grid.push([0,0,0,0,0,0,0,0,0])
+  }
 
   var arr = generateRandom9();
   var usedIndex = [];
   for (let row = 0; row < 9; row++){
     for (let col = 0; col < 9; col++){
       if(row < 3 && col < 3){
+
         let rindex = Math.floor(Math.random() * 9);
         while(usedIndex.includes(rindex)){
           rindex = Math.floor(Math.random() * 9);
         }
+        console.log(`${row} and ${col}`);
         grid[row][col] = arr[rindex];
+
         usedIndex.push(rindex);
+
       }else if (row < 6 && col < 6 && row > 2 && col > 2){
-        usedIndex = [];
+        if(row == 3 && col == 3){
+          usedIndex = [];
+        }
+
         let rindex = Math.floor(Math.random() * 9);
         while(usedIndex.includes(rindex)){
           rindex = Math.floor(Math.random() * 9);
         }
         grid[row][col] = arr[rindex];
         usedIndex.push(rindex);
+
       }else if(row < 9 && col < 9 && row > 5 && col > 5){
-        usedIndex = [];
+
+        if(row == 6 && col == 6){
+          usedIndex = [];
+        }
+
         let rindex = Math.floor(Math.random() * 9);
         while(usedIndex.includes(rindex)){
           rindex = Math.floor(Math.random() * 9);
         }
         grid[row][col] = arr[rindex];
-        usedIndex = [];
+        usedIndex.push(rindex);
+
       }else{
         grid[row][col] = 0;
       }
     }
   }
-
-
-  // for (let row = 0; row < 9; row++){
-  //   for (let col = 0; col < 9; col++){
-  //     if (grid[row][col] == 0){
-  //       arrHorizontal = 
-  //       for (let i = 1; i < 10; i++){
-
-  //         if (i ){
-
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-
-  
-
-
-
   return grid;
 }
 
 function displayGridOnSite(){
 
   const sudokuGrid = generateSudokuGrid();
-  var cell = 1;
-  var box = 1; // a box is a 3x3 idk if this is the actual term ¯\_(ツ)_/¯
+  var cell = 0;
+  var box = 0; // a box is a 3x3 idk if this is the actual term ¯\_(ツ)_/¯
 
-  for (let i = 0; i < 9; i++){
-    for (let j = 0; j < 9; j++){
-      
-      let inputCell = document.querySelector(`.s${box} .g${cell}`)
-      inputCell.value = sudokuGrid[i][j];
-      cell++;
+  for (box; box < 9; box++){
+    for (let i = 1; i < 4; i++){
+      for (let j = 1; j < 4; j++){
+        switch (box) {
+          case 0:
+            var inputCell = document.querySelector(`.s${i} .g${j}`);
+            inputCell.value = sudokuGrid[box][cell];
+            cell++
+            break;
+          case 1:
+            var inputCell = document.querySelector(`.s${i} .g${j+3}`);
+            inputCell.value = sudokuGrid[box][cell];
+            cell++
+            break;
+          case 2:
+            var inputCell = document.querySelector(`.s${i} .g${j+6}`);
+            inputCell.value = sudokuGrid[box][cell];
+            cell++
+            break;
+          case 3:
+            var inputCell = document.querySelector(`.s${i+3} .g${j}`);
+            inputCell.value = sudokuGrid[box][cell];
+            cell++
+
+            break;
+          case 4:
+            var inputCell = document.querySelector(`.s${i+3} .g${j+3}`);
+            inputCell.value = sudokuGrid[box][cell];
+            cell++
+            break;
+          case 5:
+            var inputCell = document.querySelector(`.s${i+3} .g${j+6}`);
+            inputCell.value = sudokuGrid[box][cell];
+            cell++
+            break;
+          case 6:
+            var inputCell = document.querySelector(`.s${i+6} .g${j}`);
+            inputCell.value = sudokuGrid[box][cell];
+            cell++
+            break;
+          case 7:
+            var inputCell = document.querySelector(`.s${i+6} .g${j+3}`);
+            inputCell.value = sudokuGrid[box][cell];
+            cell++
+            break;
+          case 8:
+            var inputCell = document.querySelector(`.s${i+6} .g${j+6}`);
+            inputCell.value = sudokuGrid[box][cell];
+            cell++
+            break;
+          
+        }
+        
+      }
     }
-    box++;
-    cell=1;
+    cell=0;
   }
   return sudokuGrid;
 }
-//easter egg
-//2easter egg
+
+
 
 let sudokuGrid = displayGridOnSite();
 console.log(sudokuGrid);
